45b899ede70f1f3354bbeb11b2afac34aaba2299
Ryan Haywood <ryan@ryanhaywood.com>
2014-05-04 23:57:56 -0400

    Created function to form the 2D grid of tiles

--- src/tiels/core.cljs
+++ src/tiels/core.cljs
@@ -4,17 +4,25 @@
 
 (enable-console-print!)
 
-(def tile {:width 5 :height 15})
+(def app-state (atom {:tiles []}))
 
-(def app-state (atom {:tiles [{:width 5
-                               :height 15
-                               :bgColor "red"
-                               :color "white"}
-                              {:width 5
-                               :height 15
-                               :bgColor "blue"
-                               :color "white"}]}))
+;; Default tile
+(def tile {:width 5
+           :height 15
+           :bgColor "red"
+           :color "white"})
 
+(defn tile-row
+  "Creates a vector of n tiles"
+  [n]
+  (into [] (take n (repeat tile))))
+
+(defn make-grid
+  "Creates a 2D vector of n rows and n col in each row"
+  [row col]
+  (into [] (take row (repeat (tile-row col)))))
+
+;; This has to change
 (defn tile-component [tile owner]
   (reify
     om/IRender
@@ -27,6 +35,7 @@
                    :className "tile"}
               (dom/span #js {:className "dot"} ".")))))
 
+;; This has to change
 (defn stage-view [app owner]
   (reify
     om/IRender
@@ -36,13 +45,17 @@
                                      :border "1px solid gray"
                                      :margin "0 auto"}}))))
 
+;; Component that initializes the UI
 (defn app-view [app owner]
   (reify
     om/IRender
     (render [this]
             (om/build stage-view app))))
 
-(om/root
-  app-view
-  app-state
-  {:target (. js/document (getElementById "app"))})
+;; Make the 2D grid of default tiles
+(swap! app-state assoc :tiles (make-grid 25 50))
+
+;; render app
+(om/root app-view
+         app-state
+         {:target (. js/document (getElementById "app"))})
