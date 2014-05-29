1bb836e27d2d9e4c8f2f0c6d43506ce12472a357
Ryan Haywood <ryan@ryanhaywood.com>
2014-05-29 08:26:02 -0400

    Refactored the legend, at parity with the progess made in the JS app

--- src/tiels/core.cljs
+++ src/tiels/core.cljs
@@ -4,35 +4,47 @@
 
 (enable-console-print!)
 
-(defn make-tile-current [tile selected-tile]
-  (om/update! tile selected-tile))
+(defn make-tile-current [tile _ {:keys [selected-tile] :as opts}]
+  (om/update! tile @selected-tile))
 
-(defn tile [tile owner]
+(defn make-tile-selected [tile _ {:keys [selected-tile] :as opts}]
+  (om/update! selected-tile @tile))
+
+(defn tile [tile owner {:keys [selected-fn selected-tile] :as opts}]
   (reify
     om/IRender
     (render [_]
-            (let [selected-tile (om/get-shared owner :selected-tile)
-                  is-selected (= selected-tile tile)]
-              (dom/div #js {:onMouseDown #(make-tile-current tile selected-tile)
-                            :style #js {:width 8
-                                        :height 15
-                                        :backgroundColor (:bgcolor tile)}
-                            :className (str "tile" (if is-selected
-                                                     " is-selected-tile"
-                                                     ""))}
-                       (dom/span #js {:className "circle"
-                                      :style #js {:backgroundColor (:color tile)}}
-                                 ""))))))
+            (dom/div #js {:onMouseDown #(selected-fn tile owner opts)
+                          :style #js {:width 8
+                                      :height 15
+                                      :backgroundColor (:bgcolor tile)}
+                          :className (str "tile" (if (:is-selected-tile tile)
+                                                   " is-selected-tile"
+                                                   ""))}
+                     (dom/span #js {:className "circle"
+                                    :style #js {:backgroundColor (:color tile)}}
+                               "")))))
 
-
-(defn grid [tiles owner {:keys [width] :as opts}]
+(defn grid [app owner {:keys [width] :as opts}]
   (reify
     om/IRender
     (render [_]
             (apply dom/div #js {:className "grid"
                                 :style #js {:margin "0 auto"
                                             :width width}}
-                   (om/build-all tile tiles)))))
+                   (om/build-all tile (:tiles app)
+                      {:opts {:selected-tile (:selected-tile app)
+                              :selected-fn make-tile-current}})))))
+
+(defn legend [app owner]
+  (reify
+    om/IRender
+    (render [_]
+            (apply dom/div #js {:style #js {:display "flex"}}
+                     (om/build-all tile (:tiles app)
+                                   {:fn #(assoc % :is-selected-tile (= % (:selected-tile app)))
+                                    :opts {:selected-tile (:selected-tile app)
+                                           :selected-fn make-tile-selected}})))))
 
 ;; Component that initializes the UI
 (defn app-view [app owner]
@@ -40,14 +52,17 @@
     om/IRender
     (render [_]
             (dom/div #js {:style #js {:display "flex"}}
-                     (om/build grid (:tiles app)
+                     (om/build legend {:tiles (:legend-tiles app)
+                                       :selected-tile (:selected-tile app)})
+                     (om/build grid {:tiles (:tiles app)
+                                     :selected-tile (:selected-tile app)}
                                {:opts {:width (* 8
                                                  (get-in app [:grid :columns]))}})))))
 
 (defn create-tiles [n]
   (into [] (take n (repeat {:bgcolor "white" :color "red"}))))
 
-(def app-state (atom {:tiles (create-tiles (* 60 30))
+(def app-state (atom {:tiles []
                       :grid {:rows 30 :columns 60}
                       :selected-tile {:bgcolor "cyan"  :color "blue"}
                       :legend-tiles [{:bgcolor "#444"   :color "white"}
@@ -57,8 +72,10 @@
                                      {:bgcolor "pink"   :color "white"}
                                      {:bgcolor "yellow" :color "red"}]}))
 
+(swap! app-state assoc :tiles (create-tiles (* (get-in @app-state [:grid :rows])
+                                               (get-in @app-state [:grid :columns]))))
+
 ;; render app
 (om/root app-view
          app-state
-         {:target (. js/document (getElementById "app"))
-          :shared {:selected-tile (:selected-tile @app-state)}})
+         {:target (. js/document (getElementById "app"))})
