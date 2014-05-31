37913f3b4498a36f6156804d914510f7b30c7bd6
Ryan Haywood <ryan@ryanhaywood.com>
2014-05-30 23:52:07 -0400

    Can now create types of tiles, styled legend

--- src/tiels/core.cljs
+++ src/tiels/core.cljs
@@ -35,29 +35,83 @@
     om/IRender
     (render [_]
             (apply dom/div #js {:className "grid"
-                                :style #js {:margin "0 auto"
-                                            :width width}}
+                                :style #js {:width width}}
                    (om/build-all tile (:tiles app)
                       {:opts {:selected-tile (:selected-tile app)
                               :selected-fn make-tile-current}})))))
 
-(defn history-view [app _]
+(defn undo-view [_ _]
   (reify
     om/IRender
     (render [_]
             (dom/div #js {:className "undo"
                           :onMouseDown #(undo)}
-                     "↩"))))
+                     "⌫"))))
+
+(defn new-tile [_ owner {:keys [create-fn] :as opts}]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:value ""})
+
+    om/IRenderState
+    (render-state [_ state]
+      (let [value (:value state)]
+        (dom/input #js {:className "new-tile"
+                        :value value
+                        :onChange (fn [e]
+                                    (let [new-value (aget e "target" "value")]
+                                      (om/set-state! owner :value new-value)))
+                        :onKeyUp (fn [e]
+                                    (let [return-key (= 13 (aget e "which"))]
+                                      (when return-key
+                                        (do
+                                          (om/set-state! owner :value "")
+                                          (create-fn value)))))})))))
+
 
 (defn legend [app owner]
   (reify
     om/IRender
     (render [_]
-            (apply dom/div #js {:style #js {:display "flex"}}
+            (apply dom/div #js {:className "legend"}
                      (om/build-all tile (:tiles app)
                                    {:fn #(assoc % :is-selected-tile (= % (:selected-tile app)))
                                     :opts {:selected-tile (:selected-tile app)
                                            :selected-fn make-tile-selected}})))))
+
+(defn toolbar [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+       {:creating-new-tile false})
+
+    om/IRenderState
+    (render-state [_ state]
+                  (dom/div #js {:className "toolbar"}
+                           (om/build legend {:tiles (:legend-tiles app)
+                                             :selected-tile (:selected-tile app)})
+
+                           (dom/div #js {:className "actions"}
+                             (om/build undo-view {})
+                             (dom/div #js {:className (str "new-tile-icon"
+                                                           (when (om/get-state owner :creating-new-tile)
+                                                             " is-active"))
+                                           :onMouseDown
+                                             #(om/set-state! owner :creating-new-tile
+                                               (not (om/get-state owner :creating-new-tile)))}
+                                      "░"))
+
+                           (when (:creating-new-tile state)
+                             (om/build new-tile {}
+                                       {:opts {:create-fn
+                                               (fn [value]
+                                                 (let [legend-tiles (:legend-tiles @app)
+                                                       new-legend-tile {:bgcolor value :color "white"}
+                                                       new-legend-tiles (conj (:legend-tiles @app)
+                                                                          {:bgcolor value :color "white"})]
+                                                   (when (nil? (some #(= % new-legend-tile) legend-tiles))
+                                                     (om/update! app :legend-tiles new-legend-tiles))))}}))))))
 
 ;; Component that initializes the UI
 (defn app-view [app owner]
@@ -65,11 +119,7 @@
     om/IRender
     (render [_]
             (dom/div #js {:style #js {:display "flex"}}
-                     (dom/div #js {:style #js {:display "flex"
-                                               :flexDirection "column"}}
-                              (om/build legend {:tiles (:legend-tiles app)
-                                                :selected-tile (:selected-tile app)})
-                              (om/build history-view app))
+                     (om/build toolbar app)
                      (om/build grid {:tiles (:tiles app)
                                      :selected-tile (:selected-tile app)}
                                {:opts {:width (* 10
@@ -79,7 +129,7 @@
   (into [] (take n (repeat {:bgcolor "white" :color "red"}))))
 
 (def app-state (atom {:tiles []
-                      :grid {:rows 28 :columns 45}
+                      :grid {:rows 30 :columns 50}
                       :selected-tile {:bgcolor "cyan"  :color "blue"}
                       :legend-tiles [{:bgcolor "#444"   :color "white"}
                                      {:bgcolor "blue"   :color "white"}
@@ -98,6 +148,7 @@
          app-state
          {:tx-listen
           (fn [data state]
+            (println "FUCKASDFSD")
             (when-not (= (last @history) @state)
               (swap! history conj (:old-state data))))
           :target (. js/document (getElementById "app"))})
